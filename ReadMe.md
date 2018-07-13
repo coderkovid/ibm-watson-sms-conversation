d18cc03918d3f328d82b5339e9fd0ca2e5a970c1
KaVeer Panthi <kvr24611@gmail.com>
2018-07-13 18:55:39 +0545

    IBM Watson SMS Conversation

---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+IBM Watson Conversation App with Twilio SMS
+
+Steps:
+1. Download both the files.
+2. Upload watsonworkspace.json file to ibm watson assistant workspace
+3. Go to cmd and then ,
+	pip install twilio
+	pip install flask
+	pip install watson-devloper-cloud.
+4. Run watson.py.
+5. Open CMD then,
+	ssh -R 80:localhost:5000 serveo.net
+6. Create your twilio account.
+7. Go to Twilio Dashboard > Phone Numbers and in SMS section add your serveo link shown in cmd /sms.
+8. Send SMS from your phone to your twilio number.
+9. Get Reply.
+
+Share with all of your friends.
+
+Video Link:-
---
+++ watson.py
@@ -0,0 +1,64 @@
+import watson_developer_cloud
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+	return '<a href="/sms">SMS</a>'
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    # Start our TwiML response
+    from_number = request.values.get('From')
+    country = request.values.get('FromCountry')
+    message_body = request.values.get('Body')
+    print(request.values)
+    print('{} has sent a message saying {} from {}'.format(str(from_number), str(message_body), str(country)))
+    service = watson_developer_cloud.AssistantV1(
+    username = 'd88afd7b-08e3-468f-9877-c9cc73745ed7', # replace with service username
+    password = 'oqdfG87Nu32U', # replace with service password
+    version = '2018-02-16'
+          )
+    workspace_id = '54e4534b-07c8-4504-9afa-a46bcdad8039'
+    user_input = str(message_body)
+    context = {}
+    while True:
+            response = service.message(
+                    workspace_id = workspace_id,
+                    input = {
+                    'text': user_input
+                     },
+            context = context
+                    )
+
+            if response['intents']:
+                    print('Detected intent: #' + response['intents'][0]['intent'])
+
+            # Print the output from dialog, if any.
+            
+            
+            if response['output']['text']:
+                    resp = MessagingResponse()
+                    resp.message(str(response['output']['text'][0]))
+                    return str(resp)
+
+            # Update the stored context with the latest received from the dialog.
+            context = response['context']
+
+    # Prompt for next round of input.
+
+
+    
+
+    
+
+
+  
+
+
+app.run(debug=True)
+
